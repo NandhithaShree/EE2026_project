1a8546a8f9549cbebc7d1f2c9f4b25105304fcd1
BevLow <lowbeverlypersonal@gmail.com>
2025-04-10 19:47:11 +0800

    Update screens to new design

--- FDP.srcs/sources_1/new/Frame_Renderer.v
+++ FDP.srcs/sources_1/new/Frame_Renderer.v
@@ -3,9 +3,11 @@
 
 module Frame_Renderer (
     input [6:0] pixel_x, pixel_y,
-    input [1023:0] FRAME,
+    input [3071:0] FRAME,
+    input hover,
     output reg [15:0] oled_data        
 );
+    
     localparam H_START = 16;  // Starting X position (centered on 96-pixel wide display)
     localparam V_START = 0;   // Starting Y position
     localparam SCALE = 2;     // Each bit of the frame is displayed as 2x2 pixels
@@ -19,11 +21,26 @@ module Frame_Renderer (
     wire [4:0] scaled_y = pixel_y / SCALE;
     
     // Calculate the frame index with horizontal flip
-    wire [9:0] frame_index = (31 - scaled_y) * 32 + (31 - scaled_x);   
+    wire [9:0] frame_index = (31 - scaled_y) * 32 + (31 - scaled_x);
+    wire [3:0] pixel_color = FRAME[frame_index*3 +: 3];   
         
     always @(pixel_x, pixel_y) begin 
         if (in_bounds)
-            oled_data = (FRAME[frame_index] == B) ? WHITE : BLACK;
+            case (pixel_color)
+                B: oled_data = BLACK;
+                W: oled_data = WHITE;
+                G: oled_data = LIGHT_GREEN;
+                P: oled_data = LIGHT_PINK;
+                R: oled_data = DIRT_RED;
+                S: oled_data = GREY;
+                H: oled_data = hover ? DARK_BROWN_SCREEN_VGA : WHITE;
+                LG: oled_data = LIGHT_GREEN_SCREEN;
+                DG: oled_data = DARK_GREEN_SCREEN;
+                GG: oled_data = GREEN_GREEN_SCREEN;
+                DB: oled_data = DARK_BROWN_SCREEN;
+                LB: oled_data = LIGHT_BROWN_SCREEN;
+                default: oled_data = BLACK;  // fallback
+            endcase
         else
             oled_data = BLACK;
     end
--- FDP.srcs/sources_1/new/VGA_colour_mapping.v
+++ FDP.srcs/sources_1/new/VGA_colour_mapping.v
@@ -28,13 +28,18 @@ module VGA_colour_mapping (
 
     always @(*) begin 
         case (oled_data)
-            WHITE:        vga_data = WHITE_VGA;
-            BLACK:        vga_data = BLACK_VGA;
-            LIGHT_BROWN:  vga_data = LIGHT_BROWN_VGA;
-            DARK_BROWN:   vga_data = DARK_BROWN_VGA;
-            GREEN:        vga_data = GREEN_VGA;
-            BLUE:         vga_data = BLUE_VGA;
-            default:      vga_data = 12'h000;  // fallback to black
+            WHITE:                vga_data = WHITE_VGA;
+            BLACK:                vga_data = BLACK_VGA;
+            LIGHT_BROWN:          vga_data = LIGHT_BROWN_VGA;
+            DARK_BROWN:           vga_data = DARK_BROWN_VGA;
+            GREEN:                vga_data = GREEN_VGA;
+            BLUE:                 vga_data = BLUE_VGA;
+            LIGHT_BROWN_SCREEN:   vga_data = LIGHT_BROWN_SCREEN_VGA;
+            DARK_BROWN_SCREEN:    vga_data = DARK_BROWN_SCREEN_VGA;
+            GREEN_GREEN_SCREEN:   vga_data = GREEN_GREEN;
+            LIGHT_GREEN_SCREEN:   vga_data = LIGHT_GREEN_VGA;
+            DARK_GREEN_SCREEN:    vga_data = DARK_GREEN_VGA;
+            default:              vga_data = 12'h000;  // fallback to black
         endcase
     end
 
