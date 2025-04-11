a2eb066f5801763bb3b180845ca4e8040e3dabc9
TVageesan <59349293+TVageesan@users.noreply.github.com>
2025-04-11 21:52:03 +0800

    Update Current_Piece.v

--- FDP.srcs/sources_1/new/Current_Piece.v
+++ FDP.srcs/sources_1/new/Current_Piece.v
@@ -1,11 +1,12 @@
-`timescale 1ns / 1ps
-
 module Current_Piece(
     input [255:0] board,
     input [3:0] x, y,
-    output [3:0] piece
+    output reg [3:0] piece
 );
     wire [7:0] idx;
     assign idx = ((7-y) * 8 + x) * 4; // Convert grid coords to bit position
-    assign piece = board[idx+:4];  // Get 4 bits at calculated index
+    always @ (*) begin
+        if (y < 0 || y > 7 || x < 0 || x > 7) piece = 4'b0000;
+        else piece = board[idx+:4];  // Get 4 bits at calculated index
+    end
 endmodule
